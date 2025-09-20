974ac216cf07b1c3e4be0ce10bf6740ab15226e4
AtsushiUtsumi <debtgx103@gmail.com>
2025-09-20 15:28:42 +0900

    コンフリクト解消

--- sugumi_web_alpha/urls.py
+++ sugumi_web_alpha/urls.py
@@ -4,7 +4,27 @@ from django.http import HttpResponse
 from django.urls import path
 
 from sugumi_web_alpha import views
+import yfinance as yf
 
+def get_stock_price(symbol, date):
+    """指定されたシンボルと日付の株価を取得する関数"""
+    # 昨日の日付を取得
+    yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+    # yfinanceで株価データ取得
+    df = yf.download(symbol, period="5d", interval="1d")
+    print(type(df))
+    try:
+        price = int(df.loc[yesterday]['Close'][0] * 10) / 10
+    except Exception:
+        price = None
+    return price
+
+def kabu(request):
+    symbol = "4755.T"
+    price = get_stock_price(symbol, datetime.now())
+    if price is None:
+        return HttpResponse("株価情報の取得または解析に失敗しました")
+    return HttpResponse(f"{symbol}の株価は{price}円です")
 
 # ↓ New basic view returning "Hello, Fly!" ↓
 def hello(request):
@@ -12,13 +32,17 @@ def hello(request):
     return HttpResponse(django.get_version())
 
 from django.shortcuts import render
+import requests
+from django.http import HttpResponse
+from datetime import datetime, timedelta
 
 def menu(request):
     return render(request,'menu/index.html')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    #path("", hello, name="hello"),
+    path("kabu/", kabu, name="kabu"),
+    path("hello/", hello, name="hello"),
     path("menu/", menu, name="menu"),
     path('', views.index, name='index'),
 ]
